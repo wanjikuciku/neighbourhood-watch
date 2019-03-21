b637ed3662f28006ec8cecea93970795a1fce0ee
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-03-21 16:31:23 +0300

    add login hrmls and reg forms

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lorna/Documents/django-workout/neighbourhood-watch/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
