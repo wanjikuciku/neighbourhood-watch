ea193792ac35a9178d2635c1543f88eeb42ccbe1
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-03-22 12:19:57 +0300

    add models

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http  import HttpResponse,Http404
+from .models import Neighbourhood,Business,UserProfile
+
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
