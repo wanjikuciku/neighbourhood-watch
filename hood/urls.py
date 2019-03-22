b53004da8d6f7b403ac642e528fc5891ffac553d
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-03-22 12:19:42 +0300

    add models

--- hood/urls.py
+++ hood/urls.py
@@ -1,8 +1,11 @@
 from . import views
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns=[
+
+    url(r'^$',views.index, name='index'),
 
 ]
