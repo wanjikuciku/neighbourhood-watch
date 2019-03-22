fab1379d0e7d72c9d0c4fc061af1ed89e67afdae
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-03-22 12:21:00 +0300

    add models

--- watch/settings.py
+++ watch/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap3',
+    'hood',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,8 +117,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+#login redirect
+LOGIN_REDIRECT_URL = "home"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
